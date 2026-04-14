f9d12e927eb16df72d2b98955389ca2fd14ce574
Hammam <hmaamalhndwan@gmail.com>
2026-04-13 17:23:03 -0900

    new update

--- auth-backend/temp.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.room.findMany().then(res => console.dir(res.map(r => ({ name: r.name, image: r.image })), { depth: null })).finally(() => prisma.$disconnect());
